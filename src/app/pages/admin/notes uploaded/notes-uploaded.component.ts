f8f72d4c2142a8f3123b930b92be18c798ddb9aa
Shafiqah12 <nazababies.gmail.com>
2025-07-07 01:50:50 +0800

    huhu

--- src/app/pages/admin/notes uploaded/notes-uploaded.component.ts
+++ src/app/pages/admin/notes uploaded/notes-uploaded.component.ts
@@ -8,8 +8,6 @@ import { Subscription, Observable, of } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { Router } from '@angular/router';
 
-const connectDB = require('./notesyncs_db');
- connectDB();
 
 // Define an interface for your Note structure for better type safety
 interface Note {
